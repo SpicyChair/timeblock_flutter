4bdbce797c666ce5802b80c12a1280b8117b33c1
Ethan <63197802+SpicyChair@users.noreply.github.com>
2022-08-20 11:39:44 +0100

    Create saved_day.g.dart

---
+++ lib/model/saved_day.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'saved_day.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class SavedDayAdapter extends TypeAdapter<SavedDay> {
+  @override
+  final int typeId = 2;
+
+  @override
+  SavedDay read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return SavedDay()..intervals = (fields[0] as Map).cast<int, String>();
+  }
+
+  @override
+  void write(BinaryWriter writer, SavedDay obj) {
+    writer
+      ..writeByte(1)
+      ..writeByte(0)
+      ..write(obj.intervals);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SavedDayAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
